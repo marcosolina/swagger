29fd0166b335d49bfcfe7844bd69f04acff976d4
Marco <marcosolina@gmail.com>
2020-11-28 12:36:31 +0100

    adding headers

--- swaggertests/src/main/java/com/marco/swagger/controllers/MainController.java
+++ swaggertests/src/main/java/com/marco/swagger/controllers/MainController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,33 +31,49 @@ import io.swagger.annotations.ApiOperation;
 public class MainController {
     private static Map<Integer, ArticleDto> articles = new HashMap<Integer, ArticleDto>();
     private static int counterId;
+    
+    static {
+        ArticleDto dto = new ArticleDto();
+        dto.setId(counterId);
+        dto.setTitle("Default Article");
+        dto.setDesc("Default description");
+        articles.put(counterId, dto);
+    }
 
     @PostMapping
     @ApiOperation(value = "It stores the new article", code = 201)
-    public ResponseEntity<ArticleDto> insert(@RequestBody InsertArticleDto article) {
+    public ResponseEntity<ArticleDto> insert(@RequestHeader HttpHeaders headers, @RequestBody InsertArticleDto article) {
         ArticleDto dto = new ArticleDto();
-        counterId++;
-        dto.setId(counterId);
+        dto.setId(counterId++);
         dto.setTitle(article.getTitle());
         dto.setDesc(article.getDesc());
         
         articles.put(counterId, dto);
-        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+        
+        
+        HttpHeaders respHeaders = generateSomeHeaders();
+        respHeaders.addAll(headers);
+        
+        return new ResponseEntity<>(dto, respHeaders, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     @ApiOperation(value = "It returns the article with the matching id", code = 200)
-    public ResponseEntity<ArticleDto> get(@PathVariable("id") Integer id) {
+    public ResponseEntity<ArticleDto> get(@RequestHeader HttpHeaders headers, @PathVariable("id") Integer id) {
         ArticleDto dto = articles.get(id);
         if (dto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(dto, HttpStatus.OK);
+        
+        HttpHeaders respHeaders = generateSomeHeaders();
+        respHeaders.addAll(headers);
+        
+        return new ResponseEntity<>(dto, respHeaders, HttpStatus.OK);
     }
 
     @GetMapping("/all")
     @ApiOperation(value = "It returns the list of available articles", code = 200)
-    public ResponseEntity<ArticlesResponse> getAll() {
+    public ResponseEntity<ArticlesResponse> getAll(@RequestHeader HttpHeaders headers) {
 
         Set<Entry<Integer, ArticleDto>> set = articles.entrySet();
         List<ArticleDto> list = new ArrayList<ArticleDto>();
@@ -67,28 +85,46 @@ public class MainController {
         ArticlesResponse resp = new ArticlesResponse();
         resp.setArticles(list);
 
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+        HttpHeaders respHeaders = generateSomeHeaders();
+        respHeaders.addAll(headers);
+        
+        return new ResponseEntity<>(resp, respHeaders, HttpStatus.OK);
     }
 
     @PutMapping()
     @ApiOperation(value = "It updates the article", code = 200)
-    public ResponseEntity<ArticleDto> update(@RequestBody ArticleDto article) {
+    public ResponseEntity<ArticleDto> update(@RequestHeader HttpHeaders headers, @RequestBody ArticleDto article) {
         ArticleDto dto = articles.get(article.getId());
         if (dto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         articles.put(article.getId(), article);
-        return new ResponseEntity<>(article, HttpStatus.OK);
+        
+        HttpHeaders respHeaders = generateSomeHeaders();
+        respHeaders.addAll(headers);
+        
+        return new ResponseEntity<>(article, respHeaders, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation(value = "It deletes the article", code = 204)
-    public ResponseEntity<ArticleDto> delete(@PathVariable("id") Integer id) {
+    public ResponseEntity<ArticleDto> delete(@RequestHeader HttpHeaders headers, @PathVariable("id") Integer id) {
         ArticleDto dto = articles.get(id);
         if (dto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         articles.remove(id);
-        return new ResponseEntity<>(dto, HttpStatus.OK);
+        
+        HttpHeaders respHeaders = generateSomeHeaders();
+        respHeaders.addAll(headers);
+        
+        return new ResponseEntity<>(dto, respHeaders, HttpStatus.OK);
     }
+    
+    private HttpHeaders generateSomeHeaders() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("marco-header", "marco-header-value");
+        return responseHeaders;
+    }
+    
 }
